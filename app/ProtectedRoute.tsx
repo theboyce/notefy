a7ab01e3564fe2c076b781e0ee03159c668ffbd0
sadiquex <abubakasaddik1@gmail.com>
2024-06-06 21:58:27 +0000

    feat: protected routes

---
+++ app/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+// "use client";
+// import React, { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "./firebase";
+
+// const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+//   const [user, loading] = useAuthState(auth);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const checkAuthentication = async () => {
+//       if (!loading) {
+//         if (!user) {
+//           // Redirect to the home page if the user is not authenticated
+//           router.push("/");
+//         }
+//       }
+//     };
+
+//     checkAuthentication();
+//   }, [loading, user, router]);
+
+//   return <>{children}</>;
+// };
+
+// export default ProtectedRoute;
